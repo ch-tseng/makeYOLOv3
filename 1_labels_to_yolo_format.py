7beca6897bfbe7e8fb2ff7bedcc25a661a0e5775
ch-tseng <ch.tseng@sunplusit.com>
2019-01-17 21:35:11 +0800

    update

--- 1_labels_to_yolo_format.py
+++ 1_labels_to_yolo_format.py
@@ -5,14 +5,15 @@ from shutil import copyfile
 import cv2
 from xml.dom import minidom
 from os.path import basename
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+#from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 
 #--------------------------------------------------------------------
 folderCharacter = "/"  # \\ is for windows
-xmlFolder = "../datasets/cucumber_A/labels"
-imgFolder = "../datasets/cucumber_A/images"
-saveYoloPath = "../datasets/cucumber_A/yolo"
-classList = { "0_cucumber_flower":0, "2_cucumber_matured": 1 }
+xmlFolder = "/media/sf_datasets/categories/breads_POS/labels"
+imgFolder = "/media/sf_datasets/categories/breads_POS/images"
+negFolder = "/media/sf_datasets/categories/breads_POS/negatives"
+saveYoloPath = "/media/sf_datasets/categories/breads_POS/yolo"
+classList = { "b01a":0, "b01b":1, "b01c":2, "b02":3, "b03":4, "b04":5, "b05":6, "b06":7, "b07": 8, "b08": 9, "b09": 10, "b10": 11, "b11": 12 }
 
 #---------------------------------------------------------------------
 
@@ -21,65 +22,74 @@ if not os.path.exists(saveYoloPath):
 
 def transferYolo( xmlFilepath, imgFilepath, labelGrep=""):
     global imgFolder
-    
+
     img_file, img_file_extension = os.path.splitext(imgFilepath)
     img_filename = basename(img_file)
     print(imgFilepath)
-    img = cv2.imread(imgFilepath)
-    imgShape = img.shape
-    print (img.shape)
-    img_h = imgShape[0]
-    img_w = imgShape[1]
 
-    labelXML = minidom.parse(xmlFilepath)
-    labelName = []
-    labelXmin = []
-    labelYmin = []
-    labelXmax = []
-    labelYmax = []
-    totalW = 0
-    totalH = 0
-    countLabels = 0
+    if(xmlFilepath is not None):
+        img = cv2.imread(imgFilepath)
+        imgShape = img.shape
+        print (img.shape)
+        img_h = imgShape[0]
+        img_w = imgShape[1]
 
-    tmpArrays = labelXML.getElementsByTagName("filename")
-    for elem in tmpArrays:
-        filenameImage = elem.firstChild.data
+        labelXML = minidom.parse(xmlFilepath)
+        labelName = []
+        labelXmin = []
+        labelYmin = []
+        labelXmax = []
+        labelYmax = []
+        totalW = 0
+        totalH = 0
+        countLabels = 0
 
-    tmpArrays = labelXML.getElementsByTagName("name")
-    for elem in tmpArrays:
-        labelName.append(str(elem.firstChild.data))
+        tmpArrays = labelXML.getElementsByTagName("filename")
+        for elem in tmpArrays:
+            filenameImage = elem.firstChild.data
 
-    tmpArrays = labelXML.getElementsByTagName("xmin")
-    for elem in tmpArrays:
-        labelXmin.append(int(elem.firstChild.data))
+        tmpArrays = labelXML.getElementsByTagName("name")
+        for elem in tmpArrays:
+            labelName.append(str(elem.firstChild.data))
 
-    tmpArrays = labelXML.getElementsByTagName("ymin")
-    for elem in tmpArrays:
-        labelYmin.append(int(elem.firstChild.data))
+        tmpArrays = labelXML.getElementsByTagName("xmin")
+        for elem in tmpArrays:
+            labelXmin.append(int(elem.firstChild.data))
 
-    tmpArrays = labelXML.getElementsByTagName("xmax")
-    for elem in tmpArrays:
-        labelXmax.append(int(elem.firstChild.data))
+        tmpArrays = labelXML.getElementsByTagName("ymin")
+        for elem in tmpArrays:
+            labelYmin.append(int(elem.firstChild.data))
 
-    tmpArrays = labelXML.getElementsByTagName("ymax")
-    for elem in tmpArrays:
-        labelYmax.append(int(elem.firstChild.data))
+        tmpArrays = labelXML.getElementsByTagName("xmax")
+        for elem in tmpArrays:
+            labelXmax.append(int(elem.firstChild.data))
 
-    yoloFilename = saveYoloPath + folderCharacter + img_filename + ".txt"
-    print("writeing to {}".format(yoloFilename))
+        tmpArrays = labelXML.getElementsByTagName("ymax")
+        for elem in tmpArrays:
+            labelYmax.append(int(elem.firstChild.data))
 
-    with open(yoloFilename, 'a') as the_file:
-        i = 0
-        for className in labelName:
-            if(className==labelGrep or labelGrep==""):
-                classID = classList[className]
-                x = (labelXmin[i] + (labelXmax[i]-labelXmin[i])/2) * 1.0 / img_w 
-                y = (labelYmin[i] + (labelYmax[i]-labelYmin[i])/2) * 1.0 / img_h
-                w = (labelXmax[i]-labelXmin[i]) * 1.0 / img_w
-                h = (labelYmax[i]-labelYmin[i]) * 1.0 / img_h
+        yoloFilename = saveYoloPath + folderCharacter + img_filename + ".txt"
+        print("writeing to {}".format(yoloFilename))
 
-                the_file.write(str(classID) + ' ' + str(x) + ' ' + str(y) + ' ' + str(w) + ' ' + str(h) + '\n')
-                i += 1
+        with open(yoloFilename, 'a') as the_file:
+            i = 0
+            for className in labelName:
+                if(className==labelGrep or labelGrep==""):
+                    classID = classList[className]
+                    x = (labelXmin[i] + (labelXmax[i]-labelXmin[i])/2) * 1.0 / img_w 
+                    y = (labelYmin[i] + (labelYmax[i]-labelYmin[i])/2) * 1.0 / img_h
+                    w = (labelXmax[i]-labelXmin[i]) * 1.0 / img_w
+                    h = (labelYmax[i]-labelYmin[i]) * 1.0 / img_h
+
+                    the_file.write(str(classID) + ' ' + str(x) + ' ' + str(y) + ' ' + str(w) + ' ' + str(h) + '\n')
+                    i += 1
+
+    else:
+        yoloFilename = saveYoloPath + folderCharacter + img_filename + ".txt"
+        print("writeing negative file to {}".format(yoloFilename))
+
+        with open(yoloFilename, 'a') as the_file:
+            the_file.write('')
 
     the_file.close()
 
@@ -102,3 +112,12 @@ for file in os.listdir(imgFolder):
 
             transferYolo( xmlfile, imgfile, "")
             copyfile(imgfile, saveYoloPath + folderCharacter + file)
+
+for file in os.listdir(negFolder):
+    filename, file_extension = os.path.splitext(file)
+    file_extension = file_extension.lower()
+    imgfile = negFolder + folderCharacter + file
+
+    if(file_extension == ".jpg" or file_extension==".png" or file_extension==".jpeg" or file_extension==".bmp"):
+        transferYolo( None, imgfile, "")
+        copyfile(imgfile, saveYoloPath + folderCharacter + file)
