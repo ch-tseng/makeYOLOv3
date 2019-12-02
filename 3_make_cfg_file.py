668d2254f11e8841649d4ad072700247e5faad5a
ch-tseng <ch.tseng@sunplusit.com>
2019-12-02 23:09:57 +0800

    update

--- 3_make_cfg_file.py
+++ 3_make_cfg_file.py
@@ -1,35 +1,36 @@
 import os
 
 #-------------------------------------------------------------
-classes = 12
+classes = 13
 
 #Same with you defined in 1_labels_to_yolo_format.py
-classList = { "a1":0, "a2":1, "a3":2, "a4":3, "a5":4, "a6":5, "a7":6, "a8":7, "a9": 8, "a10": 9, "a11": 10, "a12": 11 }
+classList = { "person_head":0, "person_vbox":1, "person_fbox":2 }
 folderCharacter = "/"  # \\ is for windows
-cfgFolder = "/WORK1/dataset/breads_20191125/cfg.breads_20191125"
+cfgFolder = "/WORK1/dataset/breads_20191125_v2/cfg.breads_20191125"
 
 #-------------------------------------------------------------
 
 cfg_obj_names = "obj.names"
 cfg_obj_data = "obj.data"
 
-if not os.path.exists(cfgFolder + folderCharacter + "weights"):
-    os.makedirs(cfgFolder + folderCharacter + "weights")
-    print("all weights will generated in here: " + cfgFolder + folderCharacter + "weights" + folderCharacter)
+pathCFG = os.path.join(cfgFolder, "weights")
+if not os.path.exists(pathCFG):
+    os.makedirs(pathCFG)
+    print("all weights will generated in here: ", pathCFG)
 
 
-with open(cfgFolder + folderCharacter + cfg_obj_data, 'w') as the_file:
+with open(os.path.join(cfgFolder, cfg_obj_data), 'w') as the_file:
     the_file.write("classes= " + str(classes) + "\n")
-    the_file.write("train  = " + cfgFolder + folderCharacter + "train.txt\n")
-    the_file.write("valid  = " + cfgFolder + folderCharacter + "test.txt\n")
-    the_file.write("names = " + cfgFolder + folderCharacter + "obj.names\n")
-    the_file.write("backup = " + cfgFolder + folderCharacter + "weights/")
+    the_file.write("train  = " + os.path.join(cfgFolder ,"train.txt") + "\n")
+    the_file.write("valid  = " + os.path.join(cfgFolder ,"test.txt") + "\n")
+    the_file.write("names = " + os.path.join(cfgFolder ,"obj.names") + "\n")
+    the_file.write("backup = " + os.path.join(cfgFolder ,"weights") + "/")
 
 the_file.close()
 
-print("and cfg folder: " + cfgFolder + " ,is ready for training.")
+print("and cfg folder: " + pathCFG + " ,is ready for training.")
 
-with open(cfgFolder + folderCharacter + cfg_obj_names, 'w') as the_file:
+with open(os.path.join(cfgFolder ,cfg_obj_names), 'w') as the_file:
     for className in classList:
         the_file.write(className + "\n")
 
